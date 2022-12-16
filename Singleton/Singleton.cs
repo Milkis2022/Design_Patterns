bf8692fc9d03b0c49febede3a7be63fd593a5e75
Milkis2022 <108617300+Milkis2022@users.noreply.github.com>
2022-12-16 23:15:33 +0900

    Update Singleton.cs

--- Singleton/Singleton.cs
+++ Singleton/Singleton.cs
@@ -1,1 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns
+{
+    public sealed class Singleton
+    {
+       
+        private Singleton() { }
+
+       
+        private static Singleton _instance;
+
+      
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+
+        public void someBusinessLogic()
+        {
+            // ...
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The client code.
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Singleton works, both variables contain the same instance.");
+            }
+            else
+            {
+                Console.WriteLine("Singleton failed, variables contain different instances.");
+            }
+        }
+    }
+}
 

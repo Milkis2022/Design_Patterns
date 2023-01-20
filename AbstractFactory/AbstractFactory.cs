733201da98dea7fe5bb513bba39da4dcf75b1b72
Milkis2022 <108617300+Milkis2022@users.noreply.github.com>
2023-01-20 14:29:58 +0900

    Update AbstractFactory.cs

--- AbstractFactory/AbstractFactory.cs
+++ AbstractFactory/AbstractFactory.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace RefactoringGuru.DesignPatterns.AbstractFactory.Conceptual
+namespace AbstractFactory
 {
     // The Abstract Factory interface declares a set of methods that return
     // different abstract products. These products are called a family and are

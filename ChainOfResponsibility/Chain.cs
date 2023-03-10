a495dc812812c0914d753006f034f16bae2823b5
Milkis2022 <108617300+Milkis2022@users.noreply.github.com>
2023-03-10 23:07:19 +0900

    Update Chain.cs

--- ChainOfResponsibility/Chain.cs
+++ ChainOfResponsibility/Chain.cs
@@ -2,5 +2,10 @@ using System;
 
 namespace ChainOfResponsibility
 {
-
+    public interface IHandler
+    {
+        IHandler SetNext(IHandler handler);
+        
+        object Handle(object request);
+    }
 }

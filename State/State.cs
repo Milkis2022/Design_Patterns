a3d18b727f93519f6bb84a4bb122fcd713f02f50
Milkis2022 <108617300+Milkis2022@users.noreply.github.com>
2023-04-15 22:20:27 +0900

    Update State.cs

--- State/State.cs
+++ State/State.cs
@@ -43,4 +43,44 @@ namespace State
         public abstract void Handle1();
         public abstract void Handle2();
     }
+
+    class ConcreteStateA : State
+    {
+        public override void Handle1()
+        {
+            Console.WriteLine("ConcreteStateA handles request1.");
+            Console.WriteLine("ConcreteStateA wants to change the state of the context.");
+            this._context.TransitionTo(new ConcreteStateB());
+        }
+
+        public override void Handle2()
+        {
+            Console.WriteLine("ConcreteStateA handles request2.");
+        }
+    }
+
+    class ConcreteStateB : State
+    {
+        public override void Handle1()
+        {
+            Console.Write("ConcreteStateB handles request1.");
+        }
+
+        public override void Handle2()
+        {
+            Console.WriteLine("ConcreteStateB handles request2.");
+            Console.WriteLine("ConcreteStateB wants to change the state of the context.");
+            this._context.TransitionTo(new ConcreteStateA());
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The client code.
+            var context = new Context(new ConcreteStateA());
+            context.Request1();
+            context.Request2();
+        }
+    }
 }

23deb0c78f4e7c0c69d79a3f01d0f685a941b4b1
Milkis2022 <108617300+Milkis2022@users.noreply.github.com>
2023-04-21 22:51:38 +0900

    Update Strategy.cs

--- Strategy/Strategy.cs
+++ Strategy/Strategy.cs
@@ -1,15 +1,85 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Strategy
 {
-class Context
-{
+    class Context
+    {
+        private IStrategy strategy;
 
-}
+        public Context() { }
 
-public interface IStrategy
-{
+        public Context(IStrategy strategy)
+        {
+            this.strategy = strategy;
+        }
 
-}
+        public void SetStrategy(IStrategy strategy)
+        {
+            this.strategy = strategy;
+        }
 
+        public void DoSomeBusinessLogic()
+        {
+            Console.WriteLine("Context: Sorting data using the strategy (not sure how it'll do it)");
+            var result = this.strategy.DoAlgorithm(new List<string> { "a", "b", "c", "d", "e" });
+
+            string resultStr = string.Empty;
+            foreach (var element in result as List<string>)
+            {
+                resultStr += element + ",";
+            }
+
+            Console.WriteLine(resultStr);
+        }
+
+    }
+
+    interface IStrategy
+    {
+        object DoAlgorithm(object data);
+    }
+
+    class ConcreteStrategyA : IStrategy
+    {
+        public object DoAlgorithm(object data)
+        {
+            var list = data as List<string>;
+            list.Sort();
+
+            return list;
+        }
+    }
+    class ConcreteStrategyB : IStrategy
+    {
+        public object DoAlgorithm(object data)
+        {
+            var list = data as List<string>;
+            list.Sort();
+            list.Reverse();
+
+            return list;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new Context();
+
+            Console.WriteLine("Client: Strategy is set to normal sorting.");
+            context.SetStrategy(new ConcreteStrategyA());
+            context.DoSomeBusinessLogic();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Client: Strategy is set to reverse sorting.");
+            context.SetStrategy(new ConcreteStrategyB());
+            context.DoSomeBusinessLogic();
+        }
+    }
 }
